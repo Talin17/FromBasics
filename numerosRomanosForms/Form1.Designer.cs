ae99b13d908ffb36d28fae247407518ef0d9fe0a
talin17 <rdpendulum@gmail.com>
2022-06-02 15:34:55 -0400

    numeroROmanoForm

---
+++ numerosRomanosForms/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace numerosRomanosForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMonto = new System.Windows.Forms.Label();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.txtRomano = new System.Windows.Forms.TextBox();
+            this.labelRomano = new System.Windows.Forms.Label();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelMonto
+            // 
+            this.labelMonto.AutoSize = true;
+            this.labelMonto.Location = new System.Drawing.Point(30, 34);
+            this.labelMonto.Name = "labelMonto";
+            this.labelMonto.Size = new System.Drawing.Size(99, 15);
+            this.labelMonto.TabIndex = 0;
+            this.labelMonto.Text = "Inserte el numero";
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(36, 55);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(117, 23);
+            this.txtMonto.TabIndex = 1;
+            // 
+            // txtRomano
+            // 
+            this.txtRomano.Location = new System.Drawing.Point(190, 55);
+            this.txtRomano.Name = "txtRomano";
+            this.txtRomano.Size = new System.Drawing.Size(143, 23);
+            this.txtRomano.TabIndex = 2;
+            // 
+            // labelRomano
+            // 
+            this.labelRomano.AutoSize = true;
+            this.labelRomano.Location = new System.Drawing.Point(190, 34);
+            this.labelRomano.Name = "labelRomano";
+            this.labelRomano.Size = new System.Drawing.Size(99, 15);
+            this.labelRomano.TabIndex = 3;
+            this.labelRomano.Text = "Numero Romano";
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(134, 125);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertir.TabIndex = 4;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 237);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.labelRomano);
+            this.Controls.Add(this.txtRomano);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.labelMonto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelMonto;
+        private TextBox txtMonto;
+        private TextBox txtRomano;
+        private Label labelRomano;
+        private Button btnConvertir;
+    }
+}
---
+++ numerosRomanosForms/Form1.cs
@@ -0,0 +1,124 @@
+
+namespace numerosRomanosForms
+{
+    public partial class Form1 : Form
+    {
+        string getUnidad(int unidad)
+        {
+            switch (unidad)
+            {
+                case 1:
+                    return "I";
+                case 2:
+                    return "II";
+                case 3:
+                    return "III";
+                case 4:
+                    return "IV";
+                case 5:
+                    return "V";
+                case 6:
+                    return "VI";
+                case 7:
+                    return "VII";
+                case 8:
+                    return "VIII";
+                case 9:
+                    return "IX";
+
+                default:
+                    return "";
+
+            }
+        }
+        string getDecena(int decena)
+        {
+            switch (decena)
+            {
+                case 1:
+                    return "X";
+                case 2:
+                    return "XX";
+                case 3:
+                    return "XXX";
+                case 4:
+                    return "XL";
+                case 5:
+                    return "L";
+                case 6:
+                    return "LX";
+                case 7:
+                    return "LXX";
+                case 8:
+                    return "LXXX";
+                case 9:
+                    return "XC";
+
+                default:
+                    return "";
+
+            }
+        }
+        string getCentena(int centena)
+        {
+            switch (centena)
+            {
+                case 1:
+                    return "C";
+                case 2:
+                    return "CC";
+                case 3:
+                    return "CCC";
+                case 4:
+                    return "CD";
+                case 5:
+                    return "D";
+                case 6:
+                    return "DC";
+                case 7:
+                    return "DCC";
+                case 8:
+                    return "DCCC";
+                case 9:
+                    return "CM";
+                default:
+                    return "";
+            }
+        }
+
+        int unidad = 0;
+        int decena = 0;
+        int centena = 0;
+        int numero = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        public void Operacion()
+        {
+
+            numero = Convert.ToInt32(txtMonto.Text);
+
+            centena = (numero / 100) * 100;
+            decena = numero - centena;
+            unidad = numero - centena;
+            centena = centena / 100;
+            decena = decena / 10;
+            unidad = unidad - decena * 10;
+
+            Convert.ToString(centena);
+            Convert.ToString(decena);
+            Convert.ToString(unidad);
+            txtRomano.Text = $"{getCentena(centena)}{getDecena(decena)}{getUnidad(unidad)}";
+        }
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            Operacion();
+        }
+    }
+}
